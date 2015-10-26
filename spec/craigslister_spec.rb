88089448da1f843c64a5e63e17603b3b4ed0a7fa
Chris Scott <christo247@gmail.com>
2015-10-26 09:36:57 -0700

    url works for areas outside of sfbay

--- spec/craigslister_spec.rb
+++ spec/craigslister_spec.rb
@@ -2,9 +2,19 @@ require 'spec_helper'
 require_relative '../craigslister'
 
 RSpec.describe Craigslister, '#url' do
-  let (:hondas) { Craigslister.new(area: 'sfbay', item: 'Honda CBR') }
   it 'builds craigslist url from arguments' do
+    hondas = Craigslister.new(item: 'Honda CBR')
+
     expect(hondas.url).to eq(
       'https://sfbay.craigslist.org/search/sss?sort=rel&query=honda+cbr')
+  end
+
+  context 'when given area argument' do
+    it 'builds url with correct area' do
+      harleys = Craigslister.new(area: 'austin', item: 'Harley Davidson')
+
+      expect(harleys.url).to eq(
+        'https://austin.craigslist.org/search/sss?sort=rel&query=harley+davidson')
+    end
   end
 end
