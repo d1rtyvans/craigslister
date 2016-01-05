202237db865bffc6edcc3bfd4fed211a01c7e7eb
Chris Scott <christo247@gmail.com>
2016-01-04 19:43:12 -0800

    Add unit tests for Scraper and add support/

---
+++ spec/scraper_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+RSpec.describe Scraper do
+  context '#links' do
+    it 'scrapes post links from a craigslist page' do
+      scraper = Scraper.new(
+        File.expand_path('support/test_page.html', File.dirname(__FILE__)), '')
+
+      expect(scraper.links).to eq([
+        './spec/support/fake_item_1.html',
+        './spec/support/fake_item_1.html',
+        './spec/support/fake_item_1.html',
+        './spec/support/fake_item_1.html'])
+    end
+
+    it 'returns properly formatted links for out of town craigslist posts' do
+      scraper = Scraper.new(
+        File.expand_path('support/out_of_town.html', File.dirname(__FILE__)), '')
+
+      expect(scraper.links.first).to eq(
+        'https://stockton.craigslist.org/mcy/5362368419.html')
+    end
+  end
+
+  context '#scrape' do
+    it 'returns an array of "Posts"' do
+      scraper = Scraper.new(
+        File.expand_path('support/test_page.html', File.dirname(__FILE__)), '')
+
+      results = scraper.scrape
+
+      expect(results.count).to eq(4)
+      expect(results[0].title).to eq('2015 Honda CBRÂ® 300R')
+      expect(results[0].image).to eq('http://images.craigslist.org/00U0U_j8CHhaGW9Ze_600x450.jpg')
+      expect(results[0].price).to eq(4399)
+      expect(results[0].location).to eq('vallejo / benicia')
+      expect(results[0].url).to eq('./spec/support/fake_item_1.html')
+      expect(results[0].description).to eq('CHECK THE DESCRIPTION')
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,8 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'nokogiri'
+require_relative '../lib/craigslister'
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -52,7 +54,7 @@ RSpec.configure do |config|
   # Allows RSpec to persist some state between runs in order to support
   # the `--only-failures` and `--next-failure` CLI options. We recommend
   # you configure your source control system to ignore this file.
-  config.example_status_persistence_file_path = 'spec/examples.txt'
+  # config.example_status_persistence_file_path = 'spec/examples.txt'
 
   # Limits the available syntax to the non-monkey patched syntax that is
   # recommended. For more details, see:
